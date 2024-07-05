35200e5f4cab682bf9861d4d0a799ee21a2bc8f7
Sourav Basu <souravbasu17@zohomail.in>
2024-07-05 22:35:22 +0530

    Added models and controllers

--- app.go
+++ app.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"triplanner/controllers"
+	"triplanner/core"
+	"triplanner/middlewares"
+
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	core.LoadEnvs()
+	core.ConnectDB()
+
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	router.POST("/auth/signup", controllers.CreateUser)
+	router.POST("/auth/login", controllers.Login)
+	router.GET("/user/profile", middlewares.CheckAuth, controllers.GetUserProfile)
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
--- controllers/auth.go
+++ controllers/auth.go
@@ -2,10 +2,13 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 	"triplanner/core"
 	"triplanner/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -41,4 +44,51 @@ func CreateUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": user})
 
+}
+
+func Login(c *gin.Context) {
+
+	var authInput models.AuthInput
+
+	if err := c.ShouldBindJSON(&authInput); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var userFound models.User
+	core.DB.Where("username=?", authInput.Username).Find(&userFound)
+
+	// if userFound.ID == 0 {
+	//     c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+	//     return
+	// }
+
+	if err := bcrypt.CompareHashAndPassword([]byte(userFound.Password), []byte(authInput.Password)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid password"})
+		return
+	}
+
+	generateToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":  userFound.ID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	token, err := generateToken.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to generate token"})
+	}
+
+	c.JSON(200, gin.H{
+		"token": token,
+	})
+}
+
+func GetUserProfile(c *gin.Context) {
+
+	user, _ := c.Get("currentUser")
+
+	c.JSON(200, gin.H{
+		"user": user,
+	})
 }
